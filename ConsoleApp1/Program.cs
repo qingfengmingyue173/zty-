e95e711b192ee03d0c649601ac7d4a8f8af65fd0
张桃源 <1401650720@qq.com>
2019-04-27 14:10:04 +0800

    心怀不满。删库跑路

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,18 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp1
-{
-	class Program
-	{
-		static void Main(string[] args)
-
-		{
-			Console.WriteLine("9改10");
-		}
-
-	}
-}
+﻿
